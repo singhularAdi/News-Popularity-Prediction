57c114c19f086d0445638a56f2cf8b181f7536fc
Aditya <aditya@pop-os.localdomain>
2020-04-28 00:53:32 +0530

    code cleanup

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ from scraper import get_links, parse_page_data
 
 
 links = get_links()
-
-
-
 data = parse_page_data(links)
 
 model = pickle.load(open("finalized_model.sav", 'rb'))
 y = model.predict(data)
+
 indices = np.argwhere(y == np.max(y)).flatten()
 print(highlight_green("\nArticles most likely to go viral:\n\n"))
 
